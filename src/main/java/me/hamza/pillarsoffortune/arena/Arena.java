f7702a82310028b0000d8a2f0ed736060804058d
Hamza <174505732+hammzar@users.noreply.github.com>
2025-02-21 23:18:12 +0100

    use @allargsconstructor for arena class

--- src/main/java/me/hamza/pillarsoffortune/arena/Arena.java
+++ src/main/java/me/hamza/pillarsoffortune/arena/Arena.java
@@ -1,28 +1,23 @@
 package me.hamza.pillarsoffortune.arena;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.Location;
 
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author Hammzar
  * @since 21/02/2025
  */
 
+@AllArgsConstructor
 @Getter @Setter
 public class Arena {
 
     private String name;
     private Location center;
     private List<Location> locations;
-
-    public Arena(String cName, Location cCenter, List<Location> cLocation) {
-        this.name = cName;
-        this.center = cCenter;
-        this.locations = cLocation;
-    }
 
 }
